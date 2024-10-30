34cd823161a9b1539bd0a39debd16cbe0e8c2c2d
ErikaContreras15 <econtrerasz@est.ups.edu.ec>
2024-10-30 10:43:45 -0500

    correccion de redireccion de paginas
    inicio,iniciarSesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-
+import { InicioComponent } from './page/inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,RouterLink,InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,13 @@
+
 import { Routes } from '@angular/router';
+import { InicioComponent } from './page/inicio/inicio.component';
 import { IniciarSesionComponent } from './page/iniciar-sesion/iniciar-sesion.component';
 import { RegistroComponent } from './page/registro/registro.component';
-import { InicioComponent } from './page/inicio/inicio.component';
 
 export const routes: Routes = [
-    { path: '', component: InicioComponent },
-    { path: 'login', component: IniciarSesionComponent },
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+    { path: 'inicio', component: InicioComponent },
+    { path: 'iniciarSesion', component: IniciarSesionComponent },
     { path: 'registro', component: RegistroComponent },
-//   { path: 'usuario', component: UsuarioComponent },
-//   { path: 'espacios-parqueadero', component: EspaciosParqueaderoComponent },
-//   { path: 'tarifas', component: TarifasComponent },
-//   { path: 'tickets', component: TicketsComponent },
-//   { path: 'facturas', component: FacturasComponent },
-//   { path: 'vehiculos', component: VehiculosComponent },
-//   { path: 'contratos', component: ContratosComponent },
-//   { path: 'horarios', component: HorariosComponent },
-];
+   
+  ];
