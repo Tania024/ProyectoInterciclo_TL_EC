fef7b62061c80b87df09455ea086a0fc0f6bb87f
ErikaContreras15 <econtrerasz@est.ups.edu.ec>
2024-10-30 08:11:03 -0500

    Creacion de paginas
    iniciar-sesion,registro,inicio

---
+++ src/app/page/iniciar-sesion/iniciar-sesion.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Usuario } from '../../../domain/Usuario';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-iniciar-sesion',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './iniciar-sesion.component.html',
+  styleUrl: './iniciar-sesion.component.scss'
+})
+export class IniciarSesionComponent {
+
+constructor(){
+
+}
+  
+}
