0b8671767e17fdc6be1bbce78c78852cd1201da1
Tania024 <84852408+Tania024@users.noreply.github.com>
2024-10-30 08:10:04 -0500

    Firebase
    Implementacion del firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"parqueadero-d2859","appId":"1:294983985685:web:c1e6ead59871eee49c3c1c","storageBucket":"parqueadero-d2859.appspot.com","apiKey":"AIzaSyB4HAx3rw9MyG_rbhM1eCRmtWvbXKyXNCo","authDomain":"parqueadero-d2859.firebaseapp.com","messagingSenderId":"294983985685"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({"projectId":"parqueadero-d2859","appId":"1:294983985685:web:c1e6ead59871eee49c3c1c","storageBucket":"parqueadero-d2859.appspot.com","apiKey":"AIzaSyB4HAx3rw9MyG_rbhM1eCRmtWvbXKyXNCo","authDomain":"parqueadero-d2859.firebaseapp.com","messagingSenderId":"294983985685"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
