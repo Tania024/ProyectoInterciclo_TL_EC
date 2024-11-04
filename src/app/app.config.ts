81285319b4711305fe0e443f63ed54254905f063
Tania024 <84852408+Tania024@users.noreply.github.com>
2024-11-03 19:42:04 -0500

    Implementacion de firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"parqueadero-d2859","appId":"1:294983985685:web:c1e6ead59871eee49c3c1c","storageBucket":"parqueadero-d2859.firebasestorage.app","apiKey":"AIzaSyB4HAx3rw9MyG_rbhM1eCRmtWvbXKyXNCo","authDomain":"parqueadero-d2859.firebaseapp.com","messagingSenderId":"294983985685"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyB4HAx3rw9MyG_rbhM1eCRmtWvbXKyXNCo",
+        authDomain: "parqueadero-d2859.firebaseapp.com",
+        projectId: "parqueadero-d2859",
+        storageBucket: "parqueadero-d2859.firebasestorage.app",
+        messagingSenderId: "294983985685",
+        appId: "1:294983985685:web:c1e6ead59871eee49c3c1c"
+    }
+  };
