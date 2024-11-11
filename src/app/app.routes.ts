b8bbdde657f03b84b18bf02998a45c0f7472aa39
ErikaContreras15 <econtrerasz@est.ups.edu.ec>
2024-11-10 23:21:56 -0500

    Nosotros
    Componente,muestra informacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { RegistroComponent } from './page/registro/registro.component';
 import { ParqueaderoComponent } from './components/parqueadero/parqueadero.component';
 import { TarifaComponent } from './components/tarifa/tarifa.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
-
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
 export const routes: Routes = [
     { path: '', redirectTo: '/inicio', pathMatch: 'full' },
     { path: 'inicio', component: InicioComponent },
@@ -14,6 +14,7 @@ export const routes: Routes = [
     { path: 'registro', component: RegistroComponent },
     { path: 'parqueadero', component: ParqueaderoComponent },
     { path: 'tarifa', component: TarifaComponent },
+    { path: 'nosotros', component: NosotrosComponent },
     { path: 'perfil', component: PerfilComponent }
    
   ];
